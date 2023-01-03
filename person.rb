bf2f64db077e2c019c81039cd4da0e94ed38227a
Shuhad786 <shuhad.loofer@gmail.com>
2023-01-03 18:05:00 +0200

    Create new decorator pattern with composition

--- person.rb
+++ person.rb
@@ -1,11 +1,14 @@
-class Person
+require './nameable'
+
+class Person < Nameable
   attr_accessor :id, :name, :age
 
   def initialize(id, name, age, parents_permission = 'Unknown')
     @id = id,
-          @name = name,
-          @age = age,
-          @parents_permission = parents_permission
+    @name = name,
+    @age = age,
+    @parents_permission = parents_permission
+    super
   end
 
   private
@@ -18,5 +21,9 @@ class Person
 
   def can_use_services?
     of_age? || @parents_permission == true
+  end
+
+  def correct_name
+    @name
   end
 end
